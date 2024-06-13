9697c34553c8900928bbcbc73b564703faa50628
Ignacio Lopez <ignacio.lopez@tenpo.cl>
2024-06-13 11:43:03 -0300

    adding consumers

--- a/src/main/java/cl/tenpo/microservice/a/Controller.java
+++ a/src/main/java/cl/tenpo/microservice/a/Controller.java
@@ -1,24 +1,32 @@
 package cl.tenpo.microservice.a;
 
-import io.micrometer.tracing.SpanName;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
 @RestController
 @Slf4j
+@AllArgsConstructor
 public class Controller {
 
-  @Autowired
   private RestClient restClient;
 
+  private KafkaTemplate<String, String> kafkaTemplate;
+
   @GetMapping("/a")
-  @SpanName("flujo de hello")
   public void hello() {
     log.info("Hello from microservice A");
-    restClient.get().uri("http://localhost:8088/b").retrieve();
+
+    restClient
+        .get()
+        .uri("http://localhost:8088/b")
+        .retrieve();
+
+    this.kafkaTemplate.send("topic", "Hello from microservice A");
+
   }
 
 }
--- b/src/main/java/cl/tenpo/microservice/b/BApplication.java
+++ b/src/main/java/cl/tenpo/microservice/b/BApplication.java
@@ -2,9 +2,12 @@ package cl.tenpo.microservice.b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class BApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BApplication.class, args);
